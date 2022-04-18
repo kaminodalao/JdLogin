42f0d782edb2210a4749e53f66b8f1895d76656d
kamino <kamino@imea.me>
2022-04-18 18:07:39 +0800

    set history router

--- src/main.js
+++ src/main.js
@@ -4,5 +4,5 @@ import router from './router'
 import Vant from 'vant';
 import 'vant/lib/index.css';
 
-const app = createApp(App).use(router).mount('#app')
+const app = createApp(App).use(router).use(router).mount('#app')
 app.use(Vant);
