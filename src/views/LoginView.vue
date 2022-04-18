6a73caa1f332172374427502fc0f9d3da9861b02
kamino <kamino@imea.me>
2022-04-18 23:22:07 +0800

    update

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,8 +1,23 @@
 <template>
   <div>
-    <p>{{ status }}</p>
-    <p>{{ running }}</p>
-    <div>
+    <h1>JdLogin</h1>
+    <p>STATUS: {{ status }}</p>
+    <van-cell-group inset>
+      <van-field
+        v-model="sendkeys"
+        center
+        clearable
+        label="输入"
+        placeholder="向服务器输入文本"
+      >
+        <template #button>
+          <van-button size="small" type="primary" @click="handleSendKey"
+            >提交</van-button
+          >
+        </template>
+      </van-field>
+    </van-cell-group>
+    <div v-if="running">
       <iframe
         class="vncbox"
         :src="`/vnc/?scale=1&path=websockify/?token=${this.task}&password=${this.task}`"
@@ -22,26 +37,42 @@ export default {
       status: null,
       running: null,
       showVnc: false,
+      sendkeys: null,
     };
   },
   mounted() {
     this.task = this.$route.params.task;
     setInterval(() => {
-      axios
-        .get("/api/status?task=" + this.task)
-        .then((response) => {
-          let data = response.data;
-          this.status = data.status;
-          this.running = data.running;
-        });
+      axios.get("/api/status?task=" + this.task).then((response) => {
+        let data = response.data;
+        this.status = data.status;
+        this.running = data.running;
+      });
     }, 1000);
+  },
+  methods: {
+    handleSendKey() {
+      if (this.sendkeys === null) {
+        return;
+      }
+      axios
+        .post("/api/sendkeys", {
+          task: this.task,
+          keys: this.sendkeys,
+        })
+        .then((response) => {
+            console.log(response.data)
+          this.sendkeys = null;
+        });
+    },
   },
 };
 </script>
 
 <style>
 .vncbox {
-  width: 100%;
-  height: 660px;
+  width: 400px;
+  height: 400px;
+  margin: 0 auto;
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,23 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'https://jdlogin.myyoo.fun/',
+        ws: true,
+        changeOrigin: true
+      },
+      '/websockify': {
+        target: 'https://jdlogin.myyoo.fun/',
+        ws: true,
+        changeOrigin: true
+      },
+      '/vnc': {
+        target: 'https://jdlogin.myyoo.fun/',
+        ws: true,
+        changeOrigin: true
+      },
+    }
+  },
 })
