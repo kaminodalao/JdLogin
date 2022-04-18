bc0410326c74ea3e92e82d2975b81d7a74279087
kamino <kamino@imea.me>
2022-04-18 18:05:39 +0800

    install vant

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Vant from 'vant';
+import 'vant/lib/index.css';
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App).use(router).mount('#app')
+app.use(Vant);
